9f4e76ee0f44e9b1264ceb91b2dd2721e0e5aa37
davidchung29 <60008214+davidchung29@users.noreply.github.com>
2021-07-26 06:18:19 -0700

    Created insertion sort algorithm.

--- main-2.py
+++ main-2.py
@@ -7,3 +7,14 @@ def selectionSort(li):
         li[j] = temp
   return li
 
+def insertion_sort(li):
+  for x in range(len(li)):
+    key = li[x]
+      
+    y = x-1
+    while y>=0 and key < li[y]:
+      li[y+1] = li[y]
+      y-=1
+    li[y+1] = key
+  print(li)
+
