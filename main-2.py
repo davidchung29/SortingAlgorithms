6c3c15d3d48ccc6bf7b4eb7df9c6729ea640e277
davidchung29 <60008214+davidchung29@users.noreply.github.com>
2021-07-26 07:05:49 -0700

    created bubble sort
    sorts array by comparing two elements in the list at the same time, then either swapping the two values or by comparing a different two depending on the values of the two elements.

--- main-2.py
+++ main-2.py
@@ -16,5 +16,12 @@ def insertion_sort(li):
       li[y+1] = li[y]
       y-=1
     li[y+1] = key
-  print(li)
+  return li
+
+def bubble_sort(li):
+  for i in range(len(li)):
+    for j in range(0, len(li)-i-1):
+      if li[j]>li[j+1]:
+        li[j],li[j+1] = li[j+1],li[j]
+  return li
 
